6b4543924ca977be07a1d6d108d65501a2f7dc19
Dave Bryson <daveb@miceda.org>
2013-05-20 17:10:41 -0500

    added temp support for intent service

--- users/db.js
+++ users/db.js
@@ -1,6 +1,5 @@
 
 // Temp in-memory db for testing only!
 module.exports = {
-    dave: {pw: "dave", vm: { ip: "127.0.0.1", port: 8003 }},
-    bob: {pw: "bob", vm: { ip: "127.0.0.1", port: 8003 }}
+    dave: {pw: "dave", vm: { ip: "127.0.0.1", port: 8003 }, intent: {ip: 'localhost', port: 8002}}
 };
