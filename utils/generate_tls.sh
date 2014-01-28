44fdc76ed9c70912d8228238adbadab1779574d2
Dave Bryson <daveb@miceda.org>
2014-01-28 06:51:11 -0600

    Added tls cert generation scripts
    Change-Id: Iee7c5c6e7a4ce160b688affa584a252781c4cfcc

--- utils/generate_tls.sh
+++ utils/generate_tls.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-openssl genrsa -out ../tls/private-key.pem 1024
-openssl req -new -key ../tls/private-key.pem -out ../tls/csr.pem
-openssl x509 -req -in ../tls/csr.pem -signkey ../tls/private-key.pem -out ../tls/public-cert.pem
+openssl genrsa -out private-key.pem 1024
+openssl req -new -key private-key.pem -out csr.pem
+openssl x509 -req -in csr.pem -signkey private-key.pem -out public-cert.pem
